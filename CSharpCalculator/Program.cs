4fd8760e51dbea3a10610ecd9127ffe4dac3bb03
Nafees01 <nafeesiqbal346@gmail.com>
2023-09-14 19:57:16 -0400

    Add and Subtract method added in Program.cs

--- CSharpCalculator/Program.cs
+++ CSharpCalculator/Program.cs
@@ -41,5 +41,8 @@ namespace CSharpCalculator
                     break;
             }
         }
+        public static double Add(double a, double b) => a + b;
+
+        public static double Subtract(double a, double b) => a - b;
     }
 }
