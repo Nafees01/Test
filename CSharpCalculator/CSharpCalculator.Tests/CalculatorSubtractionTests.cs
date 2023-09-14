509dff571fbaae2ae03a3f348f511142ea424af2
Nafees01 <nafeesiqbal346@gmail.com>
2023-09-14 19:53:00 -0400

    NotNull method content saved for Subtract

---
+++ CSharpCalculator/CSharpCalculator.Tests/CalculatorSubtractionTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace CSharpCalculator.Tests
+{
+    public class CalculatorSubtractionTests
+    {
+        [Fact]
+        public void TestSubtraction(double num1, double num2, double expected)
+        {
+            double result = Program.Subtract(num1, num2); 
+            Assert.NotNull(num1);
+
+        }
+    }
+}
