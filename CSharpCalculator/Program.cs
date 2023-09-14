745b0367f82fc9b2bde7f21d08cfcba3189b50db
Nafees01 <nafeesiqbal346@gmail.com>
2023-09-14 19:12:57 -0400

    Initial Commit

---
+++ CSharpCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CSharpCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose operation (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result: {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result: {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result: {num1 * num2}");
+                    break;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Error: Division by zero.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result: {num1 / num2}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    break;
+            }
+        }
+    }
+}
