b65074267484ea91d1c2337e8b126895eb405250
Nafees01 <nafeesiqbal346@gmail.com>
2023-09-14 19:34:09 -0400

    NotNull Method has been added for the Addition

--- CSharpCalculator/CSharpCalculator.Tests/CalculatorAdditionTests.cs
+++ CSharpCalculator/CSharpCalculator.Tests/CalculatorAdditionTests.cs
@@ -3,11 +3,17 @@ using Xunit;
 
 namespace CSharpCalculator.Tests
 {
-    public class UnitTest1
+    public class CalculatorAdditionTests
     {
         [Fact]
-        public void Test1()
+        public void TestAddition()
         {
+            double num1 = 5;
+            double num2 = 3;
+            double expected = 8;
+
+            double result = Program.Add(num1, num2); 
+            Assert.NotNull(num1);
 
         }
     }
